1142df3020038b717c9005aa205b967dfd1c28fa
mariem jbeli <jbelimariem403@gmail.com>
2025-08-21 23:40:50 +0100

    creation entity3

--- src/main/java/com/osmrh/rh/service/EmployeeService.java
+++ src/main/java/com/osmrh/rh/service/EmployeeService.java
@@ -2,10 +2,15 @@ package com.osmrh.rh.service;
 
 import com.osmrh.rh.Dtos.EmployeeDto;
 import com.osmrh.rh.model.Employee;
+import com.xdev.xdevbase.models.Action;
 import com.xdev.xdevbase.repos.BaseRepository;
 import com.xdev.xdevbase.services.impl.BaseServiceImpl;
+import com.xdev.xdevbase.utils.OSMLogger;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class EmployeeService extends BaseServiceImpl<Employee, EmployeeDto, EmployeeDto> {
@@ -15,5 +20,17 @@ public class EmployeeService extends BaseServiceImpl<Employee, EmployeeDto, Empl
     }
 
 
-
+    @Override
+    public Set<Action> actionsMapping(Employee Employee) {
+        long startTime = System.currentTimeMillis();
+        OSMLogger.logMethodEntry(this.getClass(), "actionsMapping", Employee);
+        Set<Action> actions = new HashSet<>();
+        actions.add(Action.READ);
+        actions.add(Action.UPDATE);
+        actions.add(Action.DELETE);
+        actions.add(Action.PAY);
+        OSMLogger.logMethodExit(this.getClass(), "actionsMapping", actions);
+        OSMLogger.logPerformance(this.getClass(), "actionsMapping", startTime, System.currentTimeMillis());
+        return actions;
+    }
 }
